e2cb800c87f0000247cacf57a6264e3a2c1ccba5
Jesse <jesse.caddell@wsu.edu>
2025-05-30 01:15:05 -0700

    Initial commit

---
+++ shopping-list-immer/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShoppingListWithImmer from './components/ShoppingListWithImmer';
+
+function App() {
+  return (
+      <div className="App">
+        <ShoppingListWithImmer />
+      </div>
+  );
+}
+
+export default App;
